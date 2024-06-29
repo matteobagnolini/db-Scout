020fbc8f62c9b8f5654815f00de38bc09d059a15
Matteo Bagnolini <matteobagnolini2003@gmail.com>
2024-06-29 10:55:20 +0200

    add dependencies for javafx

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,19 @@
 plugins { java }
+
+dependencies {
+    implementation 'org.apache.poi:poi:4.1.1'
+    implementation 'org.apache.poi:poi-ooxml:4.1.1'
+    testCompile group: 'junit', name: 'junit', version: '4.12'
+
+
+    // here starts JavaFX
+    implementation 'org.openjfx:javafx:14'
+
+    compile 'org.openjfx:javafx-base:14'
+    compile 'org.openjfx:javafx-graphics:14'
+    compile 'org.openjfx:javafx-controls:14'
+    compile 'org.openjfx:javafx-fxml:14'
+    compile 'org.openjfx:javafx-swing:14'
+    compile 'org.openjfx:javafx-media:14'
+    compile 'org.openjfx:javafx-web:14'
+}
